712e119ed7d66c910d0613e304e5de067a1e3cbe
AlexVotry <alexvotry@gmail.com>
2019-03-26 11:05:58 -0700

    handle form submit with props.handleSubmit(this.onSubmit)

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -8,18 +8,24 @@ class StreamCreate extends Component {
         // formProps comes from the Field component props.
         
         return (
-            <div className="ui form">
+            <div>
                 <label>{label}</label>
                 <input {...input} />
             </div>
         )
     }
 
+    onSubmit(formValues) {
+        console.log(formValues);
+        
+    }
+
     render() {
         return (
-            <form>
+            <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
                 <Field name="title" label="Enter Title" component={this.renderInput} />
                 <Field name="description" label="Enter Description" component={this.renderInput} />
+                <button className="primary" type="submit">Submit</button>
             </form>
         );
     }
