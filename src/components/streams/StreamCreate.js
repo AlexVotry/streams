504f08e08f530e332bd8030d0bbf0d94ada6f715
AlexVotry <alexvotry@gmail.com>
2019-03-26 10:36:35 -0700

    customizing form

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -3,18 +3,23 @@ import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends Component {
 
-    renderInput({ input }) {
+    renderInput({ input, label }) {
         // {input} is from formProps.input
         // formProps comes from the Field component props.
         
-        return <input {...input} />
+        return (
+            <div className="ui form">
+                <label>{label}</label>
+                <input {...input} />
+            </div>
+        )
     }
 
     render() {
         return (
             <form>
-                <Field name="title" component={this.renderInput} />
-                <Field name="description" component={this.renderInput} />
+                <Field name="title" label="Enter Title" component={this.renderInput} />
+                <Field name="description" label="Enter Description" component={this.renderInput} />
             </form>
         );
     }
