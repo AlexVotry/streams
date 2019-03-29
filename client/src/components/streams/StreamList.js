649bee2c36bd0c7384efc4d86146c4ee5102d6aa
AlexVotry <alexvotry@gmail.com>
2019-03-29 11:47:23 -0700

    make edit and delete buttons conditionally appear for user

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -8,11 +8,23 @@ class StreamList extends Component {
         this.props.fetchAllStreams();
     }
 
+    renderButtons(stream) {
+        if (this.props.currentUserId === stream.userId) {
+            return (
+                <div className="right floated content">
+                    <button className="ui button primary">Edit</button>
+                    <button className="ui button negative">Delete</button>
+                </div>
+            )
+        }
+    }
+
     renderList() {
         const streams = this.props.streams;
         return streams.map(stream => {
             return (
                 <div className="item" key={stream.id}>
+                    {this.renderButtons(stream)}
                     <i className="large middle aligned icon camera" />
                     <div className="content">
                         {stream.title}
@@ -35,8 +47,11 @@ class StreamList extends Component {
     }
 }
 
-const mapStateToProps = ({ streams }) => {
-    return { streams: Object.values(streams)};
+const mapStateToProps = (state) => {
+    return { 
+        streams: Object.values(state.streams),
+        currentUserId: state.auth.userId
+    };
 }
 
 
