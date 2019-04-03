a78d4de914a513312c7efb753dc38692f625714c
AlexVotry <alexvotry@gmail.com>
2019-04-03 14:36:19 -0700

    refactor create start on edit

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,5 @@
 import streams from '../apis/streams';
+import history from '../history';
 import { 
     SIGN_IN, 
     SIGN_OUT, 
@@ -27,6 +28,9 @@ export const createStream = formValues => async (dispatch, getState) => {
     const response = await streams.post('/streams', { ...formValues, userId });
    
     dispatch({ type: CREATE_STREAM, payload: response.data });
+    // the following line adds the root as last route to history. this puts
+    // us back to the root after this completes.
+    history.push('/');
 }
 
 export const fetchAllStreams = () => async dispatch => {
--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchOneStream, editStream } from '../../actions';
+import StreamForm from './StreamForm';
 
-const StreamEdit = () => {
-    return (
-        <div>StreamEdit</div>
-    );
+class StreamEdit extends React.Component {
+
+    componentDidMount() {
+        if (!this.props.stream) {
+            console.log(this.props.stream);
+            this.props.fetchOneStream(this.props.match.params.id);
+        }
+    }
+
+    onSubmit = (formValues) => {
+        console.log(formValues);
+    }
+
+    render() {
+        console.log(this.props);
+        if(!this.props.stream) {
+            return <div>Loading...</div>
+        }
+        return (
+            <div>
+                <h3>Edit a Stream</h3>
+                <StreamForm onSubmit={this.onSubmit} />
+            </div>
+        );
+    }
 }
 
-export default StreamEdit;
+const mapStateToProps = (state, ownProps) => {
+    const id = ownProps.match.params.id;
+    return { stream: state.streams[id] };
+}
+
+export default connect(
+    mapStateToProps, 
+    { fetchOneStream, editStream }
+)(StreamEdit);
